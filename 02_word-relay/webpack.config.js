2d165941f76fb0117a4dd0349ec461fbf892c75b
freshmmango <zmffkdn33@gmail.com>
2021-06-06 01:53:41 +0900

    2.4 모듈 시스템과 웹팩 설정

--- 02_word-relay/webpack.config.js
+++ 02_word-relay/webpack.config.js
@@ -1,3 +1,18 @@
+const path = require('path')
+
 module.exports = {
+  name: 'wordrelay-setting',
+  mode: 'development', // 실서비스: production으로 바꾸면 됨
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx'] // file 중 js, jsx extension 파일을 찾음
+  },
   
+  entry: {
+    app: ['./client']
+  }, // 입력
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js'
+  }, // 출력
 }
