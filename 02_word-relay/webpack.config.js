2f11761222d878bb8fe99263488b1e66e302f832
freshmmango <zmffkdn33@gmail.com>
2021-06-06 02:06:53 +0900

    2-5. 웹팩으로 빌드하기

--- 02_word-relay/webpack.config.js
+++ 02_word-relay/webpack.config.js
@@ -11,6 +11,17 @@ module.exports = {
   entry: {
     app: ['./client']
   }, // 입력
+
+  module: {
+    rules: [{
+      test: /\.jsx?/, // js, jsx 파일에 babel을 적용하겠다
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env', '@babel/preset-react']
+      }
+    }]
+  },
+
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'app.js'
