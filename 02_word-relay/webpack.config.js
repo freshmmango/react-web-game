1813789dea54208169cef13b449208af61b8bcd9
freshmmango <zmffkdn33@gmail.com>
2021-06-06 02:23:32 +0900

    2-7. @babel/preset-env와 plugins

--- 02_word-relay/webpack.config.js
+++ 02_word-relay/webpack.config.js
@@ -1,29 +1,45 @@
-const path = require('path')
+const path = require('path');
+const webpack = require('webpack')
 
 module.exports = {
   name: 'wordrelay-setting',
   mode: 'development', // 실서비스: production으로 바꾸면 됨
   devtool: 'eval',
   resolve: {
-    extensions: ['.js', '.jsx'] // file 중 js, jsx extension 파일을 찾음
+    extensions: ['.js', '.jsx'], // file 중 js, jsx extension 파일을 찾음
   },
-  
+
   entry: {
-    app: ['./client']
+    app: ['./client'],
   }, // 입력
 
   module: {
-    rules: [{
-      test: /\.jsx?/, // js, jsx 파일에 babel을 적용하겠다
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env', '@babel/preset-react']
-      }
-    }]
+    rules: [
+      {
+        test: /\.jsx?/, // js, jsx 파일에 babel을 적용하겠다
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', {
+                targets: {
+                  browsers: ['> 5% in KR', 'last 2 chrome versions'], // browserslist 참고
+                },
+                debug: true
+              },
+            ],
+            '@babel/preset-react',
+          ],
+        },
+      },
+    ],
   },
+  
+  plugins: [
+    new webpack.LoaderOptionsPlugin({debug: true})
+  ],
 
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'app.js'
+    filename: 'app.js',
   }, // 출력
-}
+};
