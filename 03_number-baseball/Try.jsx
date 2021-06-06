bb4eb3910a76b446d740bd416efddf2b378dd704
freshmmango <zmffkdn33@gmail.com>
2021-06-06 18:33:12 +0900

    3-13. props와 state 연결하기

--- 03_number-baseball/Try.jsx
+++ 03_number-baseball/Try.jsx
@@ -1,11 +1,19 @@
 const React = require('react');
-const { memo } = React;
+const { useState, memo } = React;
 
 const Try = memo(({ tryInfo }) => {
+  
+  // 부모에게 물려받은 props를 쓰고 싶으면 우선 state로 만든다
+  const [result, setResult] = useState(tryInfo.result)
+
+  const onClick = () => {
+    setResult('1')
+  }
+
   return (
     <li>
       <div>{tryInfo.try}</div>
-      <div>{tryInfo.result}</div>
+      <div onClick={onClick}>{result}</div>
     </li>
   )
 })
