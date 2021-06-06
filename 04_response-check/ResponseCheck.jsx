b298d542ad4cac733533633467cca42693c39a9e
freshmmango <zmffkdn33@gmail.com>
2021-06-06 19:00:57 +0900

    4-2. setTimeout 넣어 반응속도체크

--- 04_response-check/ResponseCheck.jsx
+++ 04_response-check/ResponseCheck.jsx
@@ -7,19 +7,51 @@ class ResponseCheck extends Component {
     result: [],
   }
 
-  onClickScreen = () => {
+  timeout;
+  startTime;
 
+  onClickScreen = () => {
+    // 구조분해를 해줘야 깔끔하다
+    const { state, message, result } = this.state;
+    if (state === 'waiting') {
+      this.setState({
+        state: 'ready',
+        message: '초록색이 되면 클릭하세요.'
+      })
+      this.timeout = setTimeout(() => {
+        this.setState({
+          state: 'now',
+          message: '지금 클릭!'
+        })
+        this.startTime = new Date()
+      }, Math.floor(Math.random() * 3000) + 2000) // 2 ~ 5초
+    } else if (state === 'ready') { // 성급하게 클릭
+      clearTimeout(this.timeout)
+      this.setState({
+        state: 'waiting',
+        message: '너무 성급하셨군요!'
+      })
+    } else if (state === 'now') {
+      this.endTime = new Date();
+      this.setState((prevState) => {
+        return {
+          state: 'waiting',
+          message: '클릭해서 시작하세요.',
+          result: [...prevState.result, this.endTime - this.startTime],
+        }
+      })
+    }
   }
 
   renderAverage = () => {
-    const {result} = this.state
+    const { result } = this.state
     return this.state.result.length === 0
-    ? null
-    : <div>평균 시간: {this.state.result.reduce((a, c) => a + c) / this.state.result.length} ms</div>
+      ? null
+      : <div>평균 시간: {this.state.result.reduce((a, c) => a + c) / this.state.result.length} ms</div>
   }
 
   render() {
-    const {state, message} = this.state
+    const { state, message } = this.state
     return (
       <>
         <div id="screen" className={state} onClick={this.onClickScreen}>
