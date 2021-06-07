782abc242be72977e0a4cab388fbfb826015e201
freshmmango <zmffkdn33@gmail.com>
2021-06-07 16:17:05 +0900

    8-2. createContext와 Provider

--- 08_mine-search/Form.jsx
+++ 08_mine-search/Form.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback, useContext } from 'react'
+import { TableContext, START_GAME } from './MineSearch'
 
 const Form = () => {
   const [row, setRow] = useState(10)
   const [cell, setCell] = useState(10)
   const [mine, setMine] = useState(20)
+  const {dispatch} = 
 
   const onChangeRow = useCallback((e) => {
     setRow(e.target.value)
@@ -17,7 +19,9 @@ const Form = () => {
     setMine(e.target.value)
   }, [])
 
-  const 
+  const onClickBtn = useCallback(() => {
+    dispatch({type: START_GAME, row, cell, mine})
+  }, [row, cell, mine])
 
   return (
     <div>
--- 08_mine-search/MineSearch.jsx
+++ 08_mine-search/MineSearch.jsx
@@ -1,12 +1,38 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, createContext, useMemo } from 'react';
+
+import Table from './Table'
+import Form from './Form'
+
+export const CODE = {
+  MINE: -7,
+  NORMAL : -1,
+  QUESTION: -2,
+  FLAG: -3,
+  QUESTION_MINE: -4,
+  FLAG_MINE: -5,
+  CLICKED_MINE: -6,
+  OPENED: 0 // 0 이상이면 다 opened
+}
+
+export const TableContext = createContext({
+  tableData: [],
+  dispatch: () => {}
+})
 
 const initialState = {
   tableData: [],
   timer: 0
 }
 
+export const START_GAME = 'START_GAME'
+
 const reducer = (state, action) => {
   switch (action.type) {
+    case START_GAME:
+      return {
+        ...state,
+        tableData: plantMine(action.row, action.cell, action.mine)
+      }
     default:
       return state;
   }
@@ -16,12 +42,17 @@ const MineSearch = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
   const {tableData, timer} = state
 
+  // Provider로 감싸주기
+  // 매번 새로 render되지 않게 하기 위해 useMemo 사용
+  const value = useMemo(
+    () => {tableData, dispatch}, [tableData]
+  )
   return (
-    <>
-      <Form dispatch={dispatch} />
+    <TableContext.Provider value={value}>
+      <Form />
       <div>{timer}</div>
       <Table />
-    </>
+    </TableContext.Provider>
   )
 }
 
